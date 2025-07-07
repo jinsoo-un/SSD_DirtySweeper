60d7cb1a5007032374b65514f58a1a25df4df5c4
SOOEON <bobstar0@naver.com>
2025-07-07 16:02:41 +0900

    parser red

--- TestShell/TestShell-DirtySweeper/testShell_parser_test.cpp
+++ TestShell/TestShell-DirtySweeper/testShell_parser_test.cpp
@@ -1,7 +1,30 @@
 #include "gmock/gmock.h"
 #include "testShell.cpp"
 
-TEST(ParserTest, TC1) {
-	EXPECT_EQ(1, 1);
+TEST(TestShellTest, ValidCommand_Read) {
+    MockCommandExecutor mock;
+    TestShell shell(&mock);
+
+    EXPECT_CALL(mock, read()).WillOnce(::testing::Return("READ EXECUTED"));
+
+    std::string result = shell.processCommand("read");
+    EXPECT_EQ(result, "READ EXECUTED");
 }
 
+TEST(TestShellTest, ValidCommand_Help) {
+    MockCommandExecutor mock;
+    TestShell shell(&mock);
+
+    EXPECT_CALL(mock, help()).WillOnce(::testing::Return("HELP EXECUTED"));
+
+    std::string result = shell.processCommand("help");
+    EXPECT_EQ(result, "HELP EXECUTED");
+}
+
+TEST(TestShellTest, InvalidCommand_ShouldReturnInvalid) {
+    MockCommandExecutor mock;
+    TestShell shell(&mock);
+
+    std::string result = shell.processCommand("invalid_cmd");
+    EXPECT_EQ(result, "INVALID COMMAND");
+}
