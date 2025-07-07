c750ef577fbe87cde266dbe98c005f35e4338cc5
sk.lee <moonstar11430@gmail.com>
2025-07-07 14:43:16 +0900

    [red] SSD read TC작성 TC1. Read init valid check 0x0000_0000 return을 기대 TC2. Out of range check로 -1 return 기대

--- SSD/SSD-DirtySweeper/main.cpp
+++ SSD/SSD-DirtySweeper/main.cpp
@@ -1,8 +1,25 @@
+#include <iostream>
+#include "ssd.cpp"
 #include "gmock/gmock.h"
 
+using std::string;
 
-TEST(TS, TC1)
+TEST(TS, ReadTC_InitialValue)
 {
+    SSD ssd;
+    int lba_addr = 0;
+    int acutal_data;
+    actual_data = ssd.readData(lba_addr);
+    EXPECT_EQ(0x00000000, actual_data);
+}
+
+TEST(TS, ReadTC_OutofRange)
+{
+    SSD ssd;
+    int lba_addr = 100;
+    int acutal_data;
+    actual_data = ssd.readData(lba_addr);
+    EXPECT_EQ(-1, actual_data);
 }
 
 int main()
