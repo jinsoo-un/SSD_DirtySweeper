b3740d549bc81912aec81af58db4c7a83b132175
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-07 14:48:19 +0900

    SSD Write TC(RED)

--- SSD/SSD-DirtySweeper/main.cpp
+++ SSD/SSD-DirtySweeper/main.cpp
@@ -1,9 +1,30 @@
 #include "gmock/gmock.h"
+#include "SSD.cpp"
 
 
-TEST(TS, TC1)
-{
+TEST(SSDTest, WritePass) {
+    // Arrange
+    SSD ssd;
+
+    // Act
+    bool isPass = ssd.writeData(0, 0xAAAABBBB);
+    EXPECT_TRUE(isPass);
+    
+    int readData = ssd.readData(0);
+   	EXPECT_EQ(readData, 0xAAAABBBB);
 }
+
+TEST(SSDTest, WriteFailWithOutOfAddressRange) {
+    // Arrange
+    SSD ssd;
+
+    // Act
+    bool isPass = ssd.writeData(100, 0xAAAABBBB);
+
+    // Assert
+    EXPECT_FALSE(isPass);
+}
+
 
 int main()
 {
