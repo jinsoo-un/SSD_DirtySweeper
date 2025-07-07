4ebeb0886e1d9d51da3590c27cef5da089b67d32
SOOEON <bobstar0@naver.com>
2025-07-07 16:11:27 +0900

    green cmd executor

--- TestShell/TestShell-DirtySweeper/testShell_parser_test.cpp
+++ TestShell/TestShell-DirtySweeper/testShell_parser_test.cpp
@@ -1,9 +1,21 @@
 #include "gmock/gmock.h"
 #include "testShell.cpp"
 
+class MockCommandExecutor : public CommandExecutor {
+public:
+    MOCK_METHOD(std::string, read, (), (override));
+    MOCK_METHOD(std::string, write, (), (override));
+    MOCK_METHOD(std::string, exit, (), (override));
+    MOCK_METHOD(std::string, help, (), (override));
+    MOCK_METHOD(std::string, fullRead, (), (override));
+    MOCK_METHOD(std::string, fullWrite, (), (override));
+    MOCK_METHOD(std::string, testScript, (), (override));
+};
+
 TEST(TestShellTest, ValidCommand_Read) {
     MockCommandExecutor mock;
-    TestShell shell(&mock);
+    TestShell shell;
+	shell.setExecutor(&mock);
 
     EXPECT_CALL(mock, read()).WillOnce(::testing::Return("READ EXECUTED"));
 
@@ -13,7 +25,8 @@ TEST(TestShellTest, ValidCommand_Read) {
 
 TEST(TestShellTest, ValidCommand_Help) {
     MockCommandExecutor mock;
-    TestShell shell(&mock);
+    TestShell shell;
+    shell.setExecutor(&mock);
 
     EXPECT_CALL(mock, help()).WillOnce(::testing::Return("HELP EXECUTED"));
 
@@ -23,7 +36,8 @@ TEST(TestShellTest, ValidCommand_Help) {
 
 TEST(TestShellTest, InvalidCommand_ShouldReturnInvalid) {
     MockCommandExecutor mock;
-    TestShell shell(&mock);
+    TestShell shell;
+    shell.setExecutor(&mock);
 
     std::string result = shell.processCommand("invalid_cmd");
     EXPECT_EQ(result, "INVALID COMMAND");
