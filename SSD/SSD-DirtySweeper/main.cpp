afabac40a4dcfefc2a5aa0268790666d9c061119
Kwangwon Min <kwmin96@gmail.com>
2025-07-07 14:48:59 +0900

    argparser: add tc for argparser-read

--- SSD/SSD-DirtySweeper/main.cpp
+++ SSD/SSD-DirtySweeper/main.cpp
@@ -1,8 +1,18 @@
 #include "gmock/gmock.h"
+#include "ssd.cpp"
 
+TEST(TS, ARGPARSEREAD) {
+    SSD ssd;
+    string cmd = "R 3";
+    ssd.commandParser(cmd);
+    EXPECT_EQ(2, ssd.argc);
+    EXPECT_EQ("R", ssd.op);
+    EXPECT_EQ(3, ssd.addr);
+}
 
 TEST(TS, TC1)
 {
+
 }
 
 int main()
