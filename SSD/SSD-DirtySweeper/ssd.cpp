4587ef22cfcff1d3e8e030825f36eeaffc03d56a
Kwangwon Min <kwmin96@gmail.com>
2025-07-07 14:59:36 +0900

    argparser: implement argparser-read

--- SSD/SSD-DirtySweeper/ssd.cpp
+++ SSD/SSD-DirtySweeper/ssd.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
+#include <sstream>
+#include <vector>
 
 using std::string;
 
 class SSD {
 public:
 	void commandParser(string command) {
+		std::istringstream iss(command);
+		string arg;
+		int cnt = 0;
 
+		/* scan the command line input */
+		while (iss >> arg) {
+			cnt++;
+			if (cnt == 1)
+				op = arg;
+			if (cnt == 2)
+				addr = std::stoi(arg);
+		}
+
+		argc = cnt;
 	}
 	void readData(int address) {
 
@@ -13,4 +28,8 @@ public:
 	void writeData(int address, int data) {
 
 	}
+
+	int argc;
+	string op;
+	int addr;
 };
