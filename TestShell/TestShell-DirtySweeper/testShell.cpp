99688d0488f516a1f547d6f8a4fe035fa3080c97
baencho <yuen6482@gmail.com>
2025-07-07 14:35:32 +0900

    [feat] Create Basic Read file and basic TC

--- TestShell/TestShell-DirtySweeper/testShell.cpp
+++ TestShell/TestShell-DirtySweeper/testShell.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#include <string>
+
+class SSD {
+public:
+	virtual void read(int address) = 0;
+};
 
 class TestShell {
 public:
+	TestShell(SSD* ssd) : ssd{ ssd } {}
 
+	void read(int address) {
+		ssd->read(address);
+	}
 private:
-
+	SSD* ssd;
 };
