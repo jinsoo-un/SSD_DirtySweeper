51b1b4104ba52dc781185da758772f174af71bf0
baencho <yuen6482@gmail.com>
2025-07-08 12:42:38 +0900

    [TestShell] Make writeReadAging pass tc

--- TestShell/TestShell-DirtySweeper/testShell_script_test.cpp
+++ TestShell/TestShell-DirtySweeper/testShell_script_test.cpp
@@ -6,7 +6,7 @@ TEST(ScriptTest, TC1) {
 	EXPECT_EQ(1, 1);
 }
 
-TEST(WriteReadAging, TC1) {
+TEST(WriteReadAging, CallTest) {
     testing::NiceMock<SSDMock> ssdMock;
     TestShell sut(&ssdMock);
     
@@ -23,4 +23,28 @@ TEST(WriteReadAging, TC1) {
         .Times(200);
 
     sut.writeReadAging();
+}
+
+TEST(WriteReadAging, PassTest) {
+    testing::NiceMock<SSDMock> ssdMock;
+    MockTestShell sut(&ssdMock);
+
+    const string DATA = "0xAAAABBBB";
+
+    EXPECT_CALL(ssdMock, write(0, DATA))
+        .Times(200);
+    EXPECT_CALL(ssdMock, write(99, DATA))
+        .Times(200);
+    EXPECT_CALL(ssdMock, read(0))
+        .Times(200);
+    EXPECT_CALL(ssdMock, read(99))
+        .Times(200);
+    EXPECT_CALL(sut, readOutputFile())
+        .WillRepeatedly(Return("0xAAAABBBB"));
+
+    testing::internal::CaptureStdout();
+    sut.writeReadAging();
+    std::string output = testing::internal::GetCapturedStdout();
+    cout << output;
+    EXPECT_THAT(output, ::testing::HasSubstr("PASS"));
 }
