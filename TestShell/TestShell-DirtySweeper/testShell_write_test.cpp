e7a9e115179eb8d16a98f8c659f937ffa216b93d
hyeongseok.choi <babmukja0322@gmail.com>
2025-07-08 10:45:22 +0900

    [refactor] Fixture 도입 및 TC 보강

--- TestShell/TestShell-DirtySweeper/testShell_write_test.cpp
+++ TestShell/TestShell-DirtySweeper/testShell_write_test.cpp
@@ -4,11 +4,14 @@
 using std::string;
 using namespace testing;
 
-
-TEST(TestShellWriteTest, Write)
-{
+class TestShellWriteTest : public Test {
+public:
     NiceMock< SSDMock> ssdMock;
-    TestShell sut(&ssdMock);
+    TestShell sut{ &ssdMock };
+};
+
+TEST_F(TestShellWriteTest, Write)
+{
     const int LBA = 1;
     const string DATA = "0x12345678";
     const string actual = "[Write] Done";
@@ -18,14 +21,12 @@ TEST(TestShellWriteTest, Write)
     string result = sut.write(LBA, DATA);
     EXPECT_EQ(actual, result);
 }
-TEST(TestShellWriteTest, WriteFailWithInvalidLBA)
+
+TEST_F(TestShellWriteTest, WriteFailWithInvalidLBA)
 {
-    NiceMock< SSDMock> ssdMock;
-    TestShell sut(&ssdMock);
     const int LBA = 100;
     const string DATA = "0x12345678";
     const string actual = "[Write] ERROR";
-
     EXPECT_CALL(ssdMock, write(LBA, DATA))
         .Times(1)
         .WillOnce(Return());
@@ -33,6 +34,36 @@ TEST(TestShellWriteTest, WriteFailWithInvalidLBA)
     EXPECT_CALL(ssdMock, getResult())
         .Times(1)
         .WillOnce(Return("ERROR"));
+    string result = sut.write(LBA, DATA);
+    EXPECT_EQ(actual, result);
+}
+TEST_F(TestShellWriteTest, WriteFailWithInvalidData)
+{
+    const int LBA = 100;
+    const string DATA = "Its'n not hex data";
+    const string actual = "[Write] ERROR";
+    EXPECT_CALL(ssdMock, write(LBA, DATA))
+        .Times(1)
+        .WillOnce(Return());
+
+    EXPECT_CALL(ssdMock, getResult())
+        .Times(1)
+        .WillOnce(Return("ERROR"));
+    string result = sut.write(LBA, DATA);
+    EXPECT_EQ(actual, result);
+}
+TEST_F(TestShellWriteTest, WriteFailWithInvalidOutput)
+{
+    const int LBA = 100;
+    const string DATA = "Its'n not hex data";
+    const string actual = "[Write] ERROR";
+    EXPECT_CALL(ssdMock, write(LBA, DATA))
+        .Times(1)
+        .WillOnce(Return());
+
+    EXPECT_CALL(ssdMock, getResult())
+        .Times(1)
+        .WillOnce(Return("NOT_DEFINED_RETURN_TYPE"));
     string result = sut.write(LBA, DATA);
     EXPECT_EQ(actual, result);
 }
