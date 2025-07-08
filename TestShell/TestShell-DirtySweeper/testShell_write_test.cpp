fffaf9f2530dec509f99107590c16d7bcabfafd0
hyeongseok.choi <babmukja0322@gmail.com>
2025-07-08 10:54:50 +0900

    [refactor] TC 중복코드 제거

--- TestShell/TestShell-DirtySweeper/testShell_write_test.cpp
+++ TestShell/TestShell-DirtySweeper/testShell_write_test.cpp
@@ -8,63 +8,51 @@ class TestShellWriteTest : public Test {
 public:
     NiceMock< SSDMock> ssdMock;
     TestShell sut{ &ssdMock };
+    const int VALID_LBA = 0;
+    const int INVALID_LBA = 100;
+    const string VALID_DATA = "0x12345678";
+    const string INVALID_DATA = "AHAHHAHAA";
 };
 
 TEST_F(TestShellWriteTest, Write)
 {
-    const int LBA = 1;
-    const string DATA = "0x12345678";
     const string actual = "[Write] Done";
-
-    EXPECT_CALL(ssdMock, write(LBA, DATA))
+    EXPECT_CALL(ssdMock, write(VALID_LBA, VALID_DATA))
         .Times(1);
-    string result = sut.write(LBA, DATA);
+
+    string result = sut.write(VALID_LBA, VALID_DATA);
+
     EXPECT_EQ(actual, result);
 }
 
 TEST_F(TestShellWriteTest, WriteFailWithInvalidLBA)
 {
-    const int LBA = 100;
-    const string DATA = "0x12345678";
     const string actual = "[Write] ERROR";
-    EXPECT_CALL(ssdMock, write(LBA, DATA))
+    EXPECT_CALL(ssdMock, write(INVALID_LBA, VALID_DATA))
         .Times(1)
         .WillOnce(Return());
-
     EXPECT_CALL(ssdMock, getResult())
         .Times(1)
         .WillOnce(Return("ERROR"));
-    string result = sut.write(LBA, DATA);
+
+    string result = sut.write(INVALID_LBA, VALID_DATA);
+
     EXPECT_EQ(actual, result);
 }
+
 TEST_F(TestShellWriteTest, WriteFailWithInvalidData)
 {
-    const int LBA = 100;
-    const string DATA = "Its'n not hex data";
     const string actual = "[Write] ERROR";
-    EXPECT_CALL(ssdMock, write(LBA, DATA))
+    EXPECT_CALL(ssdMock, write(INVALID_LBA, INVALID_DATA))
         .Times(1)
         .WillOnce(Return());
-
-    EXPECT_CALL(ssdMock, getResult())
+     EXPECT_CALL(ssdMock, getResult())
         .Times(1)
         .WillOnce(Return("ERROR"));
-    string result = sut.write(LBA, DATA);
-    EXPECT_EQ(actual, result);
-}
-TEST_F(TestShellWriteTest, WriteFailWithInvalidOutput)
-{
-    const int LBA = 100;
-    const string DATA = "Its'n not hex data";
-    const string actual = "[Write] ERROR";
-    EXPECT_CALL(ssdMock, write(LBA, DATA))
-        .Times(1)
-        .WillOnce(Return());
 
-    EXPECT_CALL(ssdMock, getResult())
-        .Times(1)
-        .WillOnce(Return("NOT_DEFINED_RETURN_TYPE"));
-    string result = sut.write(LBA, DATA);
+    string result = sut.write(INVALID_LBA, INVALID_DATA);
+
     EXPECT_EQ(actual, result);
 }
+
 
