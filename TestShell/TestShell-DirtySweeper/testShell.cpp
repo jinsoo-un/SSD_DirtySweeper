b9f57d70f0438a480d8754b542df08dbcd6e7c97
baencho <yuen6482@gmail.com>
2025-07-08 11:37:55 +0900

    [TestShell] write, read 200번씩 호출하도록 구현

--- TestShell/TestShell-DirtySweeper/testShell.cpp
+++ TestShell/TestShell-DirtySweeper/testShell.cpp
@@ -214,7 +214,12 @@ public:
     }
 
     void writeReadAging() {
-
+        for (int i = 0; i < 200; i++) {
+            ssd->write(0, "0xAAAABBBB");
+            ssd->read(0);
+            ssd->write(99, "0xAAAABBBB");
+            ssd->read(99);
+        }
     }
 
 private:
