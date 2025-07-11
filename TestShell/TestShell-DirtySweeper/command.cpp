da5b9405047725b103da27e976bd225d94928a19
baencho <yuen6482@gmail.com>
2025-07-11 13:03:04 +0900

    logger pritn 시 함수 이름 수정

--- TestShell/TestShell-DirtySweeper/command.cpp
+++ TestShell/TestShell-DirtySweeper/command.cpp
@@ -1,7 +1,7 @@
 ﻿#include "command.h"
 
 string ReadCommand::execute() {
-    Logger::GetInstance().print("testShell.read()", "read command called");
+    Logger::GetInstance().print("ReadCommand.execute()", "read command called");
     
     if (lba < 0 || lba > 99) {
         return TestShellOutputManager::GetInstance().getErrorReadResult();
@@ -16,7 +16,7 @@ string ReadCommand::execute() {
 
 string FullReadCommand::execute() {
     string result = "";
-    Logger::GetInstance().print("testShell.fullRead()", "full read command called");
+    Logger::GetInstance().print("FullReadCommand.execute()", "full read command called");
 
     for (int lba = LBA_START_ADDRESS; lba <= LBA_END_ADDRESS; lba++) {
         ssd->read(lba);
@@ -32,7 +32,7 @@ string FullReadCommand::execute() {
 }
 
 string WriteCommand::execute() {
-    Logger::GetInstance().print("testShell.write()", "write command called");
+    Logger::GetInstance().print("WriteCommand.execute()", "write command called");
     ssd->write(lba, data);
 
     if (isCmdExecuteError(fileAccessor->readOutputFile())) {
@@ -42,7 +42,7 @@ string WriteCommand::execute() {
 }
 
 string FullWriteCommand::execute() {
-    Logger::GetInstance().print("testShell.fullWrite()", "full write command called");
+    Logger::GetInstance().print("FullWriteCommand.execute()", "full write command called");
     for (int lba = LBA_START_ADDRESS; lba <= LBA_END_ADDRESS; lba++) {
         ssd->write(lba, data);
 
@@ -54,7 +54,7 @@ string FullWriteCommand::execute() {
 }
 
 string HelpCommand::execute() {
-    Logger::GetInstance().print("testShell.help()", "help command called");
+    Logger::GetInstance().print("HelpCommand.execute()", "help command called");
 
     string output = "Developed by: Team Members - Sooeon Jin, Euncho Bae, Kwangwon Min, Hyeongseok Choi, Yunbae Kim, Seongkyoon Lee\n";
     output += "read (LBA)         : Read data from (LBA).\n";
@@ -72,6 +72,7 @@ bool ExitCommand::isExit = false;
 
 string ExitCommand::execute() {
     isExit = true;
+    Logger::GetInstance().print("ExitCommand.execute()", "exit command called");
     return "Set Exit Comannd...\n";
 }
 
@@ -80,7 +81,7 @@ bool ExitCommand::getIsExit() {
 }
 
 string FullWriteAndReadCompareCommand::execute() {
-    Logger::GetInstance().print("testShell.fullWriteAndReadCompare()", "full write and read compare command called");
+    Logger::GetInstance().print("FullWriteAndReadCompareCommand.execute()", "full write and read compare command called");
 
     for (int lba = LBA_START_ADDRESS; lba <= LBA_END_ADDRESS; ++lba) {
         string writeData = getWriteDataInFullWriteAndReadCompareScript(lba);
@@ -105,7 +106,7 @@ string FullWriteAndReadCompareCommand::getWriteDataInFullWriteAndReadCompareScri
 }
 
 string PartialLBAWriteCommand::execute() {
-    Logger::GetInstance().print("testShell.partialLBAWrite()", "partial LBA write command called");
+    Logger::GetInstance().print("PartialLBAWriteCommand.execute()", "partial LBA write command called");
 
     const string testValue = "0x12345678";
     const int repeatCnt = 30;
@@ -140,7 +141,7 @@ string PartialLBAWriteCommand::execute() {
 }
 
 string WriteReadAgingCommand::execute() {
-    Logger::GetInstance().print("testShell.writeReadAging()", "write read aging command called");
+    Logger::GetInstance().print("WriteReadAgingCommand.execute()", "write read aging command called");
 
     for (int i = 0; i < WRITE_READ_ITERATION; i++) {
         string randomString = getRandomHexString();
@@ -195,7 +196,7 @@ bool EraseWithSizeCommand::isValidEraseWithSizeArgument(unsigned int lba, unsign
     return true;
 }
 string EraseWithSizeCommand::execute() {
-    Logger::GetInstance().print("testShell.eraseWithSize()", "erase with size command called");
+    Logger::GetInstance().print("EraseWithSizeCommand.execute()", "erase with size command called");
 
     if (!isValidEraseWithSizeArgument(lba, size)) {
         return TestShellOutputManager::GetInstance().getEraseErrorResult();
@@ -225,7 +226,7 @@ string ICommand::erase(unsigned int lba, unsigned int size) {
 }
 
 string EraseWithRangeCommand::execute() {
-    Logger::GetInstance().print("testShell.eraseWithRange()", "erase with range command called");
+    Logger::GetInstance().print("EraseWithRangeCommand.execute()", "erase with range command called");
 
     if (!isValidLbaRange(startLba, endLba)) {
         return TestShellOutputManager::GetInstance().getEraseRangeErrorResult();
@@ -257,7 +258,7 @@ bool EraseWithRangeCommand::isValidLbaRange(unsigned int startLba, unsigned int 
 }
 
 string EraseAndWriteAgingCommand::execute() {
-    Logger::GetInstance().print("testShell.eraseAndWriteAging()", "erase and write aging command called");
+    Logger::GetInstance().print("EraseAndWriteAgingCommand.execute()", "erase and write aging command called");
 
     const int eraseUnitSize = 2;
     const int maxAgingCnt = 30;
@@ -288,7 +289,7 @@ string EraseAndWriteAgingCommand::execute() {
 }
 
 string FlushCommand::execute() {
-    Logger::GetInstance().print("testShell.flushSsdBuffer()", "flush command called");
+    Logger::GetInstance().print("FlushCommand.execute()", "flush command called");
     ssd->flushSsdBuffer();
     string result = fileAccessor->readOutputFile();
     if (result == "ERROR") return TestShellOutputManager::GetInstance().getErrorFlushResult();
