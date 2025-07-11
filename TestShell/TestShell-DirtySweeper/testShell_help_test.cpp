0fcf5351cf0ca6e2f1bb11105fc90bfb1f917bf4
baencho <yuen6482@gmail.com>
2025-07-11 09:34:53 +0900

    flush test, help test 빌드 에러 수정

--- TestShell/TestShell-DirtySweeper/testShell_help_test.cpp
+++ TestShell/TestShell-DirtySweeper/testShell_help_test.cpp
@@ -2,6 +2,7 @@
 #include "logger.h"
 #include "ssd.h"
 #include "testShell.h"
+#include "command.h"
 
 using namespace testing;
 
@@ -9,14 +10,12 @@ TEST(HelpTest, HelpCallTest) {
     SSDMock ssd;
     NiceMock<MockTestShell> mockShell{ &ssd };
 
-    EXPECT_CALL(mockShell, help())
-        .Times(1);
-
-    mockShell.help();
-}
-
-TEST(HelpTest, HelpCallTest2) {
-    SSDMock ssd;
-    TestShell ts{ &ssd };
-    EXPECT_NO_THROW(ts.help());
+    string output = HelpCommand(&ssd).execute();
+    EXPECT_THAT(output, HasSubstr("read"));
+    EXPECT_THAT(output, HasSubstr("write"));
+    EXPECT_THAT(output, HasSubstr("fullread"));
+    EXPECT_THAT(output, HasSubstr("fullwrite"));
+    EXPECT_THAT(output, HasSubstr("help"));
+    EXPECT_THAT(output, HasSubstr("exit"));
+    EXPECT_THAT(output, HasSubstr("Team Members"));
 }
